0dc777e12d4d7d85214e6a6acaa5649833acd695
Nicolas <duchonic@bluewin.ch>
2019-07-12 12:44:48 +0200

    tab

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('duchoud working page');
+	res.send('duchoud working page');
+});
+
+app.get('/object', function (req, res) {
+	res.send('object oriented stuff');
 });
 
 app.listen(31898, function () {
